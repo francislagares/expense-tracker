60233a397efba536fe5c33e616937ab8439e7010
Francis Lagares <francis.lagares@gmail.com>
2021-05-29 17:06:32 +0200

    test(app): component renders without crashing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from 'App';
 
 describe('App Component', () => {
   test('Renders without crashing', () => {
     render(<App />);
-
-    const title = screen.getByRole('heading');
-
-    expect(title).toBeInTheDocument();
-    expect(title).toHaveTextContent(/hello react app/i);
   });
 });
